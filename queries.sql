32cb359fe13ba9162b301ff8b0eb7e93a1c2f8de
Michel-Ramirez <michel.ramirez19@gmail.com>
2023-07-28 14:45:42 +0200

    add

--- queries.sql
+++ queries.sql
@@ -61,3 +61,37 @@ FROM `departments`;
 SELECT * 
 FROM `teachers`
 WHERE `phone` IS NOT NULL;
+
+
+
+-- |******************************************************************| --
+
+
+
+-- 1. Contare quanti iscritti ci sono stati ogni anno
+
+SELECT COUNT(`enrolment_date`) AS `enrolment_number`, `enrolment_date`
+FROM `students`
+GROUP BY `enrolment_date`;
+
+
+
+-- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+
+SELECT COUNT(`office_number`)
+FROM `teachers`
+GROUP BY `office_number`;
+
+
+
+-- 3. Calcolare la media dei voti di ogni appello d'esame
+
+SELECT ROUND(AVG(`vote`)) AS `average_vote`, `student_id`
+FROM `exam_student`
+GROUP BY `student_id` , `vote`;
+
+
+
+-- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+
+
